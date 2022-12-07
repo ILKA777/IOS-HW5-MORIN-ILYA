22fee6a3396d7d3e10213ebca621f89c359b25c0
ILKA777 <90830436+ILKA777@users.noreply.github.com>
2022-12-07 12:44:23 +0300

    Add files via upload

---
+++ IOS-HW3-MORIN-ILYA-main 2/iomorinPW2/Controler/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  iomorinPW2
+//
+//  Created by Илья Морин on 30.11.2022.
+//
+
+import SwiftUI
+
+struct NewsViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsViewController()
+    }
+}
---
+++ IOS-HW3-MORIN-ILYA-main 2/iomorinPW2/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  iomorinPW2
+//
+//  Created by Илья Морин on 02.10.2022.
+//
+
+import UIKit
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session:
+               UISceneSession, options connectionOptions:
+               UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene)
+        else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController =
+        UINavigationController(rootViewController:
+                                WelcomeViewController())
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
